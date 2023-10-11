98af73883b995a38b95f65f6d137ebaf2c0694ff
MANISHnagarkoti <manishnagarkoti56@gmail.com>
2023-10-11 19:31:07 +0530

    s

--- server.js
+++ server.js
@@ -30,10 +30,30 @@ db()
 
 
 
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:4200'],
+  credentials: true
+}));
+
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "http://localhost:5173");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
+
+// app.use(cors({
+//   credentials: true,
+//   origin: 'http://localhost:5173'
+// }));
+
+
 app.use(express.json())
 app.use(cookieParser());
 
-app.use(cors());
+
 
 
 // app.use(morgan("dev"))
@@ -59,7 +79,7 @@ app.use("/comment", commentRoutes)
 app.get("/", (req, res) => {
 
 
-  res.send("Heloo ther]]e")
+  res.send("Heloo there")
 
 
 
