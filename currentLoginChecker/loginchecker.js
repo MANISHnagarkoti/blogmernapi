d691360735b2417a42d442462a72c4c39fa900eb
MANISHnagarkoti <manishnagarkoti56@gmail.com>
2023-10-11 07:52:37 +0530

    a

--- currentLoginChecker/loginchecker.js
+++ currentLoginChecker/loginchecker.js
@@ -6,7 +6,7 @@ const userModel = require("../models/userSchema")
 const loginAuth = (req, res) => {
 
 
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjUyNWU1MDY3MTMxZmUyMDNlOTBjY2ZkIiwiaWF0IjoxNjk2OTgyMjg3LCJleHAiOjE2OTcyNDE0ODd9.k-QQpS6X0QozESpoRnsIywoCOtyev8WpUVObphWyr_o"
+    const token = req.cookies.jwt
 
 
 
@@ -86,7 +86,7 @@ const loginAuth = (req, res) => {
         return res.json({
 
             sucess: false,
-            message: "no token focker"
+            message: "no token found"
 
         })
 
