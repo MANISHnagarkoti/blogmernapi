d447dd4e90f3f4bcda8963a050238fdadd410b52
MANISHnagarkoti <manishnagarkoti56@gmail.com>
2023-10-11 07:49:25 +0530

    fg

--- currentLoginChecker/loginchecker.js
+++ currentLoginChecker/loginchecker.js
@@ -6,7 +6,7 @@ const userModel = require("../models/userSchema")
 const loginAuth = (req, res) => {
 
 
-    const token = req.cookies.jwt
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjUyNWU1MDY3MTMxZmUyMDNlOTBjY2ZkIiwiaWF0IjoxNjk2OTgyMjg3LCJleHAiOjE2OTcyNDE0ODd9.k-QQpS6X0QozESpoRnsIywoCOtyev8WpUVObphWyr_o"
 
 
 
