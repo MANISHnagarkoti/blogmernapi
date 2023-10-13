73ed0c65e8190e59b08388510703c6dafdf4f56c
MANISHnagarkoti <manishnagarkoti56@gmail.com>
2023-10-13 23:06:33 +0530

    sss

--- server.js
+++ server.js
@@ -38,14 +38,14 @@ db()
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://tekkentagtgc.vercel.app',
   credentials: true,
   exposedHeaders: ["set-cookie"]
 }));
 
 app.use(function (req, res, next) {
 
-  res.header('Access-Control-Allow-Origin', "http://localhost:5173");
+  res.header('Access-Control-Allow-Origin', "https://tekkentagtgc.vercel.app");
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
