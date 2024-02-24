951c34fb5fa9a048c961c5377be7840c941788c7
MANISHnagarkoti <manishnagarkoti56@gmail.com>
2024-02-25 01:08:48 +0530

    s

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(cors({
 
 app.use(function (req, res, next) {
 
-  res.header('Access-Control-Allow-Origin', "http://localhost:5173");
+  res.header('Access-Control-Allow-Origin', "https://blogmernreact.vercel.app");
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
